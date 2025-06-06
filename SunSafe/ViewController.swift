d83b01fee1d21f82f97cf75d865a87151eee1fee
Egor Partenko <egorpartenko@icloud.com>
2025-06-06 18:42:39 +0200

    Initial Commit

---
+++ SunSafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SunSafe
+//
+//  Created by Егор Партенко on 6.6.25..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
